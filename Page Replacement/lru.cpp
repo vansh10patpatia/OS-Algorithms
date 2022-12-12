15943c178b1324782b4212f83d3d4bd7cfc1664e
isha0612 <77613722+isha0612@users.noreply.github.com>
2022-12-12 23:24:39 +0530

    Update lru.cpp

--- Page Replacement/lru.cpp
+++ Page Replacement/lru.cpp
@@ -1,50 +1,5 @@
-// #include<iostream>
-// #include<unordered_map>
-// using namespace std;
-
-// int main() {
-//     string s;
-//     cout<<"Enter reference string : ";
-//     cin>>s;
-//     int faults = 0, frames = 3;
-//     unordered_map<int, int> m;
-//     for(int i = 0; i < s.size(); i++) {
-//         int page = s[i] - '0';
-//         if(m.size() < frames) {
-//             if(m.find(page) == m.end()) {
-//                 faults++;
-//             }
-//             for(auto &i : m)
-//                 i.second++;
-//             m[page] = 0;
-//         } else {
-//             if(m.find(page) == m.end()) {
-//                 faults++;
-//                 int key = -1, mx = -1;
-//                 for(auto &i : m) {
-//                     if(i.second > mx) {
-//                         key = i.first;
-//                         mx = i.second;
-//                     }
-//                 }
-//                 m.erase(key);
-//                 for(auto &i : m)
-//                 i.second++;
-//                 m[page] = 0;
-//             } else {
-//                 for(auto &i : m)
-//                     i.second++;
-//                 m[page] = 0;
-//             }
-//         }
-//     }
-//     cout<<"Total faults : "<<faults;
-// }
-
-
 #include<iostream>
-#include<unordered_set>
-#include<queue>
+#include<unordered_map>
 using namespace std;
 
 int main() {
@@ -52,25 +7,36 @@ int main() {
     cout<<"Enter reference string : ";
     cin>>s;
     int faults = 0, frames = 3;
-    unordered_set<int> set;
-    deque<int> dq;
+    unordered_map<int, int> m;
     for(int i = 0; i < s.size(); i++) {
         int page = s[i] - '0';
-        if(set.size() < frames) {
-            if(set.find(page) == set.end()) {
+        if(m.size() < frames) {
+            if(m.find(page) == m.end()) {
                 faults++;
-                set.insert(page);
             }
-            dq.push_back(page);
+            for(auto &i : m)
+                i.second++;
+            m[page] = 0;
         } else {
-            if(set.find(page) == set.end()) {
+            if(m.find(page) == m.end()) {
                 faults++;
-                set.erase(dq.front());
-                dq.pop_front();
-                set.insert(page);
+                int key = -1, mx = -1;
+                for(auto &i : m) {
+                    if(i.second > mx) {
+                        key = i.first;
+                        mx = i.second;
+                    }
+                }
+                m.erase(key);
+                for(auto &i : m)
+                i.second++;
+                m[page] = 0;
+            } else {
+                for(auto &i : m)
+                    i.second++;
+                m[page] = 0;
             }
-            dq.push_back(page);
         }
     }
     cout<<"Total faults : "<<faults;
-}
+// }
